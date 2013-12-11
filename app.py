b4183b69e0d4f21ce76ab8ee1c0885bacc0631f9
Michael Schober <mynetlogins.schober@gmail.com>
2013-12-10 23:36:19 -0800

    fixed app

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app.config.update(
 )
 
 # controllers
-@app.route(/)
+@app.route("/")
 def hello():
+    return "Hello from Python!"
 
 # launch
-if __name__ == __main__:
-    port = int(os.environ.get(PORT, 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
